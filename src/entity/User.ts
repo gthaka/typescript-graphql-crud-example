058fbc94bd1b488c28ae658f514a95c39f1a3dce
Anthony G <agithaka@live.com>
2022-05-19 04:56:45 +0300

    User Entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,28 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Field, Int, ObjectType } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { LogTrails } from "./LogTrails";
 
+@ObjectType()
 @Entity()
 export class User {
+
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   firstName: string;
 
+  @Field()
   @Column()
   lastName: string;
 
+  @Field(() => Int)
   @Column()
   age: number;
+  
+  // Relationship
+  @OneToMany(() => LogTrails, logTrails => logTrails.user)
+  logTrails: LogTrails[];
 }
