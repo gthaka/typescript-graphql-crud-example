863166d4950faa1d0b43eca2269a6f7039ba33d2
Anthony G <agithaka@live.com>
2022-05-20 04:17:50 +0300

    rested

---
+++ src/resolvers/index.ts
@@ -0,0 +1,8 @@
+import { UserResolver } from "./UserResolver";
+import { HelloWorldResolver } from "./HelloWorldResolver";
+import { MovieResolver } from "./MovieResolver";
+import { LogTrailsResolver } from "./LogTrailsResolver";
+
+const Resolvers = [UserResolver, HelloWorldResolver, MovieResolver, LogTrailsResolver];
+
+export default Resolvers;
